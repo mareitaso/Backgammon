1a3895bfde0bf775062059fcde5ffc33259b83d5
Tomioka <tyuuni31@gmail.com>
2019-05-11 17:09:14 +0900

    途中経過

---
+++ Backgammon/Assets/Script/DiceController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Dice1;
+    [SerializeField]
+    GameObject Dice2;
+    [SerializeField]
+    GameObject CPUDice1;
+    [SerializeField]
+    GameObject CPUDice2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickDiceButton()
+    {
+        int Dice1Number = Random.Range(1, 7);
+        Debug.Log(Dice1Number);
+       
+        
+        int Dice2Number = Random.Range(1, 7);
+        Debug.Log(Dice2Number);
+    }
+}
---
+++ Backgammon/Assets/Script/TitleUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TitleUI : MonoBehaviour
+{
+    public void ClickOnStart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
