9d11dd8546eede56820728dc62d1037c2d747a0d
Tomioka <tyuuni31@gmail.com>
2019-07-13 18:35:00 +0900

    Fillでクリアした駒をゲージ化した

---
+++ Backgammon/Assets/Script/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image MyGauge;
+    [SerializeField]
+    private Image NPCGauge;
+
+    private float ClearPiece = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GaugeChange()
+    {
+        MyGauge.fillAmount = ClearPiece / 16;
+    }
+
+}
